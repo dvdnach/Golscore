f70d3c6afd65be599e149c7b0afad315bcf8592a
davbladm2 <davidblancoalvarez@gmail.com>
2017-05-12 20:42:06 +0200

    Cambios card categoria

--- app/src/main/java/com/example/dm2/golscore/Adapter/CategoriaAdapter.java
+++ app/src/main/java/com/example/dm2/golscore/Adapter/CategoriaAdapter.java
@@ -1,6 +1,7 @@
 package com.example.dm2.golscore.Adapter;
 
 import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,7 +34,7 @@ public class CategoriaAdapter extends RecyclerView.Adapter<CategoriaAdapter.Cate
     public void onBindViewHolder(final CategoriaViewHolder holder, final int position) {
         final Categoria s= listaCategoria.get(position);
         holder.nombreCategoriaTV.setText(s.getNombre());
-        holder.nombreCategoriaTV.setOnClickListener(new View.OnClickListener() {
+        holder.cardCategoriaCV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), GrupoActivity.class);
@@ -52,10 +53,12 @@ public class CategoriaAdapter extends RecyclerView.Adapter<CategoriaAdapter.Cate
     public static class CategoriaViewHolder extends RecyclerView.ViewHolder{
 
         private TextView nombreCategoriaTV;
+        private CardView cardCategoriaCV;
 
         public CategoriaViewHolder(View itemView) {
             super(itemView);
             nombreCategoriaTV = (TextView) itemView.findViewById(R.id.nombreCategoriaTV);
+            cardCategoriaCV=(CardView) itemView.findViewById(R.id.cardCategoriaCV);
         }
 
         public void setNombre(String nombre) {
